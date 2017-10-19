8a39068a3d3ed365a596d6584d68a2374523436d
deepnavy <petrov.evpanet@gmail.com>
2017-10-20 00:25:39 +0300

    update to nav tabs

--- assets/blog/script2.js
+++ assets/blog/script2.js
@@ -18,51 +18,29 @@ data.forEach(function(item, i, arr) {
   $("#insert-cards").append(card(item));
 });
 
-
-var styles = ["style3.css", "style4.css", "style5.css"]
-var styleNum = 0;
-
-$( "#change-style" ).click(function() {
-
-        $('head').append('<link rel="stylesheet" type="text/css" href="/assets/blog/' + styles[styleNum] + '">');
-        styleNum++;
-
-
-});
-
-$( "#remove-style" ).click(function() {
-  $('link[rel=stylesheet][href~="/assets/blog/style3.css"]').remove();
-});
-
+// make equal height of cards
 $('.card').responsiveEqualHeightGrid();
-
+// make card clickable
 $('.card').click(function() {
   var href = $(this).find('a').attr('href');
   window.location.href = href;
 });
 
-//navtabs
 
+// navtabs responsive
 $('.nav-tabs-dropdown').each(function(i, elm) {
-  
   $(elm).text($(elm).next('ul').find('li.active a').text());
-  
 });
 
 $('.nav-tabs-dropdown').on('click', function(e) {
 
   e.preventDefault();
-  
   $(e.target).toggleClass('open').next('ul').slideToggle();
-  
 });
 
 $('#nav-tabs-wrapper a[data-toggle="tab"]').on('click', function(e) {
-
   e.preventDefault();
-  
   $(e.target).closest('ul').hide().prev('a').removeClass('open').text($(this).text());
-    
 });
 
 
