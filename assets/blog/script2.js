da30f65a6fb58246ca61a98d1c832ae99bc04031
deepnavy <petrov.evpanet@gmail.com>
2017-11-03 00:29:38 +0200

    fixes

--- assets/blog/script2.js
+++ assets/blog/script2.js
@@ -11,12 +11,12 @@ data = [
 ]
 
 
-var card = _.template($("#card-template").html()); 
+// var card = _.template($("#card-template").html()); 
 
 
-data.forEach(function(item, i, arr) {
-  $("#insert-cards").append(card(item));
-});
+// data.forEach(function(item, i, arr) {
+//   $("#insert-cards").append(card(item));
+// });
 
 // make equal height of cards
 $('.equal-height').responsiveEqualHeightGrid();
@@ -42,6 +42,25 @@ $('#nav-tabs-wrapper a[data-toggle="tab"]').on('click', function(e) {
   e.preventDefault();
   $(e.target).closest('ul').hide().prev('a').removeClass('open').text($(this).text());
 });
+
+var elementsCount = $('.related-news-item').length;
+
+//choosing the grid class depending on elements count
+if (elementsCount != 0){
+
+  var elementsCount = $('.related-news-item').length;
+
+  if (elementsCount < 3) {
+    $('.related-news-item').removeClass('col-md-4 col-sm-6 ');
+    if (elementsCount == 2) {
+      $('.related-news-item').addClass('col-md-6 col-sm-6');
+    }
+    else if (elementsCount == 1) {
+      $('.related-news-item').addClass('col-md-12');
+    }
+  }
+
+}
 
 
 
